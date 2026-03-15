b0e4ddafcf84a9ce34c8ba9f500319095840d09a
Bruno Oliveira <brunostoliver@gmail.com>
2026-03-15 11:49:58 -0600

    Initial Commit

---
+++ Lactate test app_v3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lactate test app_v3
+//
+//  Created by Leonor Oliveira on 3/15/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lactate test app_v3/Lactate_test_app_v3App.swift
@@ -0,0 +1,17 @@
+//
+//  Lactate_test_app_v3App.swift
+//  Lactate test app_v3
+//
+//  Created by Leonor Oliveira on 3/15/26.
+//
+
+import SwiftUI
+
+@main
+struct Lactate_test_app_v3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
